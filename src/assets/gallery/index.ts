5c9066de9b9646bb04084e24de83ae71be20e4b1
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2026-05-18 13:17:28 +0530

    Images updated

--- src/assets/gallery/index.ts
+++ src/assets/gallery/index.ts
@@ -1,3 +1,5 @@
-export { default as Events1 } from "./Event1.jpg";
 export { default as Events2 } from "./Event2.jpg";
 export { default as Classroom1 } from "./Classroom1.jpeg";
+export { default as Classroom2 } from "./Classroom2.jpeg";
+export { default as Classroom3 } from "./Classroom3.jpeg";
+export { default as Events1 } from "./Event1.jpeg";
